2d43ca4ad67637871f1af159ae29a2dee1f12800
khaled javedan <khaledjavedan76@gmail.com>
2025-05-09 19:52:04 +0400

    feat(theme): added theme toggle logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from '@/components/mode-toggle';
+
 export default function Home() {
-  return <div className="container mx-auto">hello world</div>;
+  return (
+    <div className="container mx-auto">
+      hello world
+      <ModeToggle />
+    </div>
+  );
 }
